463c5cef9bc1c1af2c0ca44a46e5375a8b38ed4f
Harry Reeder <harry@harryreeder.co.uk>
2024-12-12 13:55:42 +0000

    add tinsel docs

--- src/python_pipelines_demo/demo.py
+++ src/python_pipelines_demo/demo.py
@@ -1,3 +1,7 @@
 def decorate_christmas_tree(color):
-    """Decorates the christmas tree"""
+    """Decorates the christmas tree
+
+    Args:
+      color: What color tinsel to use
+    """
     return f"Tree has {color} tinsel"
