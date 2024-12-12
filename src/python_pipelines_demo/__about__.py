e6883a9f9ab64b5f5fa88fff588c911dff1424ea
Harry Reeder <harry@harryreeder.co.uk>
2024-12-12 13:50:23 +0000

    demo

---
+++ src/python_pipelines_demo/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Harry Reeder <harry@harryreeder.co.uk>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/python_pipelines_demo/demo.py
@@ -0,0 +1,3 @@
+def decorate_christmas_tree(color):
+    """Decorates the christmas tree"""
+    return f"Tree has {color} tinsel"
